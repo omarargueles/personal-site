fac5be4bcd39ccf8bcba3adb381c405772cfacae
Omar Arguelles <omar00231@gmail.com>
2026-04-27 16:27:45 -0700

    header created

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      siteBuildMetadata {
+        buildTime
+      }
+    }
+  `);
+
+  return (
+    <header>
+      <h1>{data.site.siteMetadata.title}</h1>
+    </header>
+  );
+};
