3f23d565244172ac569b785a8dc20169a4d66d47
Omar Arguelles <omar00231@gmail.com>
2026-04-30 22:23:49 -0700

    improve pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <p>posts here!</p>
+    </Layout>
+  );
+};
+export default blog;
