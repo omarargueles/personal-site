51cad49f75f3cab7c73f9151d91452f124dc6d27
Omar Arguelles <omar00231@gmail.com>
2026-04-30 23:28:39 -0700

    add information with map on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,36 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
+  console.log("data", data);
   return (
-    <Layout>
-      <p>posts here!</p>
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
-const Head = () => <Seo title="My blog posts" />;
+export const query = graphql`
+  query BlogsQuery {
+    allFile {
+      nodes {
+        name
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }) => <Seo title={data.site.siteMetadata.title} />;
 
 export default BlogPage;
